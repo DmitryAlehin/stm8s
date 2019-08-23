c69ed54929060397dad2b15282d042ff2da9ff6d
DmitryAlehin <50503990+DmitryAlehin@users.noreply.github.com>
2019-08-23 03:01:42 +0300

    блинк
    проба stm8s103f3p6

---
+++ blink/main.c
@@ -0,0 +1,30 @@
+#include "stm8s.h"
+void Delay(void);
+int main( void )
+{
+  GPIO_Init(GPIOB,GPIO_PIN_5,GPIO_MODE_OUT_OD_HIZ_SLOW);
+  while(1)
+  {
+   GPIO_WriteHigh(GPIOB, GPIO_PIN_5);
+   Delay();
+   GPIO_WriteLow(GPIOB, GPIO_PIN_5);
+   Delay();
+ }
+}
+void Delay(void)
+{
+   for(uint16_t i=0;i<60000;i++)
+   {
+   
+   }
+}
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  while (1)
+  {
+    
+  }
+}
+#endif
