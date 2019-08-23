88eaf8f30086eaa93eb0681044d068d42a8e7a44
DmitryAlehin <50503990+DmitryAlehin@users.noreply.github.com>
2019-08-23 06:08:43 +0300

    обновление
    скорректированы workspace
    изменен blink с новой функцией Delay

--- blink/main.c
+++ blink/main.c
@@ -1,24 +1,41 @@
 #include "stm8s.h"
 
-void Delay(void);
+void Delay(uint32_t delay);
+void Delay(uint32_t delay);
+uint32_t TimingDelay = 0;
 
 int main( void )
 {
+  CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
   GPIO_Init(GPIOB,GPIO_PIN_5,GPIO_MODE_OUT_OD_HIZ_SLOW);
+  TIM4_TimeBaseInit(TIM4_PRESCALER_128, 124);
+  TIM4_ClearFlag(TIM4_FLAG_UPDATE);
+  TIM4_ITConfig(TIM4_IT_UPDATE, ENABLE);
+  enableInterrupts();
+  TIM4_Cmd(ENABLE);
   while(1)
   {
    GPIO_WriteHigh(GPIOB, GPIO_PIN_5);
-   Delay();
+   Delay(1000);
    GPIO_WriteLow(GPIOB, GPIO_PIN_5);
-   Delay();
+   Delay(5000);
  }
 }
-void Delay(void)
+
+INTERRUPT_HANDLER(TIM4_UPD_OVF_IRQHandler, 23)
 {
-   for(uint16_t i=0;i<60000;i++)
-   {
-   
-   }
+  if (TimingDelay)
+  {
+    TimingDelay--;
+  }
+  TIM4_ClearITPendingBit(TIM4_IT_UPDATE);
+}
+
+void Delay(uint32_t delay)
+{
+  TimingDelay = delay;
+
+  while (TimingDelay);
 }
 
 #ifdef USE_FULL_ASSERT
